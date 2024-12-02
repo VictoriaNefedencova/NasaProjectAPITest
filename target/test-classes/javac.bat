1356e40bbf3df36860ae7d6362478fe883676b8f
VictoriaNefedencova <avtootvet4ik@gmail.com>
2024-12-02 18:28:18 +0200

    Fix the logs

--- target/test-classes/javac.bat
+++
@@ -1,1 +0,0 @@
-cmd.exe /X /C "C:\Users\vnefedencova\.jdks\temurin-17.0.8.1\bin\javac.exe @C:/Users/vnefedencova/ProjectAV/Project-Demo/target/test-classes/org.codehaus.plexus.compiler.javac.JavacCompiler10284009359077638359arguments"
